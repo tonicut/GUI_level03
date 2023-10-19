03731e6497a4ef6e43d0dd0679a20b1b3e0292ce
unknown <ctonanti@gmail.com>
2023-10-19 10:08:01 -0300

    cambios

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-#pip install pyperclip
+# pip install pyperclip
 
 import tkinter as tk
 import random
 import pyperclip
+import json
 from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -28,23 +30,61 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Something went wrong!", message="Please make sure you haven't left any fields empty.")
+        messagebox.showinfo(title="Something went wrong!",
+                            message="Please make sure you haven't left any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website,
                                        message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs OK to save?")
 
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                # Updating old data with new data
+                data.update(new_data)
+
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, tk.END)
                 password_entry.delete(0, tk.END)
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data File found")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for '{website}' exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,16 +107,18 @@ password_label = tk.Label(text="Password: ")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = tk.Entry(width=40)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = tk.Entry(width=22)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = tk.Entry(width=40)
+email_entry = tk.Entry(width=41)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "@gmail.com")
 password_entry = tk.Entry(width=22)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = tk.Button(text="Search", width=15, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = tk.Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = tk.Button(text="Add", width=35, command=save)
